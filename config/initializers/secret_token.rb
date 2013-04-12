2426de26cfdf0a96601537d5b00dbbb167d84bac
Olivier Brisse <ouranos@donarproject.org>
2013-04-12 14:50:53 +1000

    init import rails + devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseTrackable::Application.config.secret_token = '0f1a3196610576f6c4c74c8019489e385ca749585ae03f2a6159097950bd3a1446b4a985b9ca3ff527e5b29a7899cd28e3ddaa7863d051f9a1cf46c94feeef62'
